bdc1c6f5407c787e9880601317f5a0dec5ff9fa4
davidsaymon <davidh_saymon@hotmail.com>
2024-07-13 01:45:18 -0300

    Trabalhando no hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss') //recupera os arquivos
@@ -7,7 +8,14 @@ function styles() {
         .pipe(gulp.dest('./dist/css')); //enviar os arquivos comprimidos para uma pasta
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*', {encoding: false}) //recupera os arquivos
+        .pipe(imagemin()) //encadear os arquivos (estilo de saida comprimido)
+        .pipe(gulp.dest('./dist/images')); //enviar os arquivos comprimidos para uma pasta
+}
+
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
